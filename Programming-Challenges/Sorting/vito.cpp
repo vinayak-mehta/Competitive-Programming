adb069a817c793988c43837a6ea0e19c9e8e2179
Vinayak Mehta <vmehta94@gmail.com>
2015-09-12 02:56:16 +0530

    Added Vito's Family

---
+++ Programming-Challenges/Sorting/vito.cpp
@@ -0,0 +1,46 @@
+/* Author: vortex_ape */
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FOR(i, a, b) for(int i = (int)a; i < (int)b; ++i)
+#define REP(i, n) FOR(i, 0, n)
+#define CLR(x) x.clear()
+#define SZ(x) ((int)(x).size())
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define INF 1000000000
+#define MOD 1000000007
+#define ALL(x) x.begin(), x.end()
+#define LET(x, a) __typeof(a) x(a)
+#define IFOR(i, a, b) for(LET(i, a); i != (b); ++i)
+#define EACH(it, v) IFOR(it, v.begin(), v.end())
+
+typedef pair<int, int> PII;
+typedef vector<PII> VPII;
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef long long LL;
+typedef unsigned long long ULL;
+
+LL mpow(LL a, LL n)
+{LL ret=1;LL b=a;while(n) {if(n&1)
+    ret=(ret*b)%MOD;b=(b*b)%MOD;n>>=1;}
+return (LL)ret;}
+
+int main()
+{
+    int T;
+    scanf("%d", &T);
+    while(T--)
+    {
+        int R, S[501], ans = 0;
+        scanf("%d", &R);
+        for(int i = 0; i < R; i++) scanf("%d", &S[i]);
+        sort(S, S + R);
+        for(int i = 0; i < R; i++) ans += abs(S[R / 2] - S[i]);
+        printf("%d\n", ans);
+    }
+    return 0;
+}
