23eb1ede94f2816cb432d4651dd09110ff60f636
Vinayak Mehta <vmehta94@gmail.com>
2015-09-18 00:14:37 +0530

    Added Number Steps

---
+++ SPOJ/NSTEPS.cpp
@@ -0,0 +1,53 @@
+/* Author: vortex_ape */
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FOR(i, a, b) for(int i = (int)a; i < (int)b; ++i)
+#define REP(i, n) FOR(i, 0, n)
+#define CLR(x) x.clear()
+#define SZ(x) ((int)(x).size())
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define INF 1000000000
+#define MOD 1000000007
+#define ALL(x) x.begin(), x.end()
+#define LET(x, a) __typeof(a) x(a)
+#define IFOR(i, a, b) for(LET(i, a); i != (b); ++i)
+#define EACH(it, v) IFOR(it, v.begin(), v.end())
+
+typedef pair<int, int> PII;
+typedef vector<PII> VPII;
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef long long LL;
+typedef unsigned long long ULL;
+
+LL mpow(LL a, LL n)
+{LL ret=1;LL b=a;while(n) {if(n&1)
+    ret=(ret*b)%MOD;b=(b*b)%MOD;n>>=1;}
+return (LL)ret;}
+
+int main()
+{
+    int N;
+    scanf("%d", &N);
+    while(N--)
+    {
+        int x, y;
+        scanf("%d%d", &x, &y);
+        if(x == y)
+        {
+            if(x % 2 == 0) printf("%d\n", x + y);
+            else printf("%d\n", x + y - 1);
+        }
+        else if(x - y == 2 && x > 1)
+        {
+            if(x % 2 == 0) printf("%d\n", x + y);
+            else printf("%d\n", x + y - 1);
+        }
+        else printf("No Number\n");
+    }
+    return 0;
+}
